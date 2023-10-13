2b74acab82ecd36dd1f5f1854d74b802fb98f5ae
ranjeeeta987 <128693464+ranjeeta-jamadar@users.noreply.github.com>
2023-10-13 19:19:46 +0530

    Add files via upload

---
+++ cicd_demo-master/cicd_demo-master/Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:8
+EXPOSE 8585
+ADD  build/libs/springboot-cicd-0.0.1-SNAPSHOT.jar  springboot-cicd-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","/springboot-cicd-0.0.1-SNAPSHOT.jar"]
---
+++ cicd_demo-master/cicd_demo-master/src/main/java/com/demo/cicd/SpringbootCicdApplication.java
@@ -0,0 +1,17 @@
+package com.demo.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdApplication.class, args);
+		
+		System.out.println("Hello world from spring boot");
+		
+		
+	}
+
+}
